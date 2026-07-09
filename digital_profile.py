c9da3e91ddf0015171c7b5687a26f1cbac2f9a5b
415-16-CelineWiegand <54632@yupparaj.ac.th>
2026-07-09 08:31:43 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title(" Mydigital proflie")
+
+st.header("แนะนำตัว")
+st.write ("นางสาวซีรินทร์ วีแกนด์")
+st.write ("ชื่อเล่น ซันนี่")
+st.write ("เลขที่ 16 ม4/15")
+
+st.header("เป้าหมายแล้วสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย อยากเป็นนางงามจักรวาล")
+st.write("อยากเป็นนางแบบ")
