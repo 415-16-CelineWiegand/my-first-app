9582be31306defb63d74dc8c4c2ab860d5a38437
415-16-CelineWiegand <54632@yupparaj.ac.th>
2026-08-18 15:51:16 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -1,20 +1,20 @@
-import streamlit as  st
+import streamlit as st
+
 st.markdown("# :red[🏋️ คำนวณค่าดัชนีมวลกาย BMI]")
 st.write("กรอกข้อมูลน้ำหนักและส่วนสูงของคุณ เพื่อเช็กสุขภาพเบื้องต้น")
-
 
 weight = st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม):", min_value=1.0)
 height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):", min_value=1.0)
 
-
 if st.button("คำนวณค่า BMI 🎯"):
-     # แปลงส่วนสูงจาก cm เป็น เมตร แล้วคำนวณ BMI
-     height_m = height_cm / 100
-     bmi = weight / (height_m ** 2)
+    height_m = height_cm / 100
+    bmi = weight / (height_m ** 2)
 
-     st.write("---")
-     st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
-  if bmi < 18.5:
+    st.write("---")
+    st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
+
+   
+    if bmi < 18.5:
         st.warning("⚠️ คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
     elif 18.5 <= bmi < 23.0:
         st.success("🎉 คุณมีน้ำหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)")
