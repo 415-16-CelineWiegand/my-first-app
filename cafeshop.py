07eb8a86c2169e989f9e68893538d3c43839143c
415-16-CelineWiegand <54632@yupparaj.ac.th>
2026-09-17 08:45:51 +0700

    Update cafeshop.py

--- cafeshop.py
+++ cafeshop.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-st.title(”Indy Cafe Menu“)
+st.title("Indy Cafe Menu")
 
-st.header(”Cafe Menu“)
+st.header("Cafe Menu")
 
-coffee = st.number_input(”Coffee - 50 Baht:“, value=0)
-cake = st.number_input(”Cake - 60 Baht:“, value=0)
-sandwich = st.number_input(”Sandwich - 70 Baht:“, value=0)
-tea = st.number_input(”Tea - 40 Baht:“, value=0)
+coffee = st.number_input("Coffee - 50 Baht:“, value=0)
+cake = st.number_input("Cake - 60 Baht:“, value=0)
+sandwich = st.number_input("Sandwich - 70 Baht:“, value=0)
+tea = st.number_input("Tea - 40 Baht:“, value=0)
 
 price = coffee  50 + cake  60 + sandwich  70 + tea  40
 
@@ -17,10 +17,10 @@ total = price + vat
 st.divider()
 
 st.header(" Bill")
-st.write(”Price:“, price, ”Baht“)
-st.write(”VAT 7%:“, vat, ”Baht“)
-st.write(”Total Price:“, total, ”Baht“)
+st.write("Price:", price, "Baht")
+st.write9"VAT 7%:", vat, "Baht")
+st.write("Total Price:", total, "Baht")
 
 st.divider()
 
-st.write(”Thank you for visiting Indy Cafe! “)
+st.write("Thank you for visiting Indy Cafe! ")
