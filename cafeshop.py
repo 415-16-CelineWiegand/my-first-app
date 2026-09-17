5f60640bbf009d5553e0548398017d309dd2584f
415-16-CelineWiegand <54632@yupparaj.ac.th>
2026-09-17 08:46:33 +0700

    Update cafeshop.py

--- cafeshop.py
+++ cafeshop.py
@@ -4,10 +4,10 @@ st.title("Indy Cafe Menu")
 
 st.header("Cafe Menu")
 
-coffee = st.number_input("Coffee - 50 Baht:“, value=0)
-cake = st.number_input("Cake - 60 Baht:“, value=0)
-sandwich = st.number_input("Sandwich - 70 Baht:“, value=0)
-tea = st.number_input("Tea - 40 Baht:“, value=0)
+coffee = st.number_input("Coffee - 50 Baht:", value=0)
+cake = st.number_input("Cake - 60 Baht:", value=0)
+sandwich = st.number_input("Sandwich - 70 Baht:", value=0)
+tea = st.number_input("Tea - 40 Baht:", value=0)
 
 price = coffee  50 + cake  60 + sandwich  70 + tea  40
 
